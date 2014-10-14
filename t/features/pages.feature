43fad12626d764f3deae51e71274446d36e54a5a
Amitai Schlair <schmonz-web-git@schmonz.com>
2014-10-13 20:43:03 -0400

    add "check index page" scenario

--- t/features/pages.feature
+++ t/features/pages.feature
@@ -10,9 +10,17 @@ Feature: Basic web pages
     Given a browser
     When I request a single article
     Then it has the sidebar
-    Then it has exactly one wordcount
     Then its title matches the slug
+    Then it has exactly one wordcount
     Then its posted date matches the slug
     Then its body content is there
 
-    #  Scenario: Check index page
+  Scenario: Check index page
+    Given a browser
+    When I request the main index page
+    Then it has the sidebar
+    Then its title matches the site title
+    Then it has five wordcounts
+    Then it has five article titles
+    Then it has five posted dates
+    # Then it has five body contents
--- t/features/step_definitions/pages_steps.pl
+++ t/features/step_definitions/pages_steps.pl
@@ -20,6 +20,10 @@ When qr/request a single article/, sub {
 	S->{browser}->get_ok(S->{baseurl} . '/2014/01/28/on-some-cusps');
 };
 
+When qr/request the main index/, sub {
+	S->{browser}->get_ok(S->{baseurl} . '/');
+};
+
 Then qr/has the sidebar/, sub {
 	S->{browser}->content_contains('<h5>About Me</h5>');
 	S->{browser}->content_contains('<h5>Greatest Hits</h5>');
@@ -27,19 +31,38 @@ Then qr/has the sidebar/, sub {
 };
 
 Then qr/exactly one wordcount/, sub {
-	my $html_content = S->{browser}->content();
-	my @wordcounts = $html_content =~ /(<h4 class="words article_atts">[0-9]+ words<\/h4>)/g;
-	is(scalar @wordcounts, 1);
+	is(count_the_wordcounts(S->{browser}->content()), 1);
 };
+
+Then qr/has five wordcounts/, sub {
+	is(count_the_wordcounts(S->{browser}->content()), 5);
+};
+
+sub count_the_wordcounts {
+	my ($html_content) = @_;
+	my @wordcounts = $html_content =~ /(<h4 class="words article_atts">[0-9]+ words<\/h4>)/g;
+	return scalar @wordcounts;
+}
 
 Then qr/title matches the slug/, sub {
 	S->{browser}->title_is(q{Yareev's schmonz.com: On some cusps});
 	S->{browser}->content_like(qr/<h3 class="article_title">.+cusps.+<\/h3>/);
 };
 
+Then qr/title matches the site title/, sub {
+	S->{browser}->title_is(q{Yareev's schmonz.com});
+};
+
+Then qr/has five article titles/, sub {
+	S->{browser}->content_like(qr/<h3 class="article_title">.+<\/h3>/);
+};
+
 Then qr/posted date matches the slug/, sub {
-	S->{browser}->text_contains('2014-01-28');
 	S->{browser}->content_contains('<h4 class="posted_date article_atts">2014-01-28 ');
+};
+
+Then qr/has five posted dates/, sub {
+	S->{browser}->content_like(qr/<h4 class="posted_date article_atts">[0-9]{4}-[0-9]{2}-[0-9]{2} /);
 };
 
 Then qr/body content is there/, sub {
